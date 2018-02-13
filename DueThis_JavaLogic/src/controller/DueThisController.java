5099c4dbbc66cf09b8187ff6e61f03c1371cf63e
aureliepluche1 <aurelie.pluche@mail.mcgill.ca>
2018-02-13 15:16:07 -0500

    Created removeEvent method

--- DueThis_JavaLogic/src/controller/DueThisController.java
+++ DueThis_JavaLogic/src/controller/DueThisController.java
@@ -69,6 +69,72 @@ public class DueThisController
 			return false;
 		}
 	}
+
+	public boolean editAssignment(Assignment anAssignment, String name, String course, Date dueDate, float gradeWeight,
+			Duration compTime, Student aStudent) throws InvalidInputException
+	{
+		java.util.Calendar cal = Calendar.getInstance();
+		java.util.Date utilDate = new java.util.Date();
+		cal.setTime(utilDate);
+		java.sql.Date sqlDate = new java.sql.Date(cal.getTime().getTime());
+
+		String error = "";
+		if (aStudent == null)
+		{
+			error += "No student is associated to this assignment! ";
+			throw new InvalidInputException(error);
+		}
+		if (name == null || name.trim().length() == 0)
+			error += "Please enter a valid assignment name! ";
+		if (course == null || course.trim().length() == 0)
+			error += "Please enter a valid course name! ";
+		if (dueDate == null)
+			error += "Due data cannot be empty! ";
+		else if (dueDate.before(sqlDate) == true)
+			error += "Due date must be in the future! ";
+		if (aStudent.getStudentRoles().size() == 0)
+			error += "Student must have a role! ";
+		else if (aStudent.getStudentRole(0) instanceof model.NoviceStudent)
+		{
+			if (gradeWeight <= 0)
+				error += "Please enter a positive grade weight! ";
+
+		} else
+		{
+			if (compTime == null)
+				error += "Please enter an estimated completion time! ";
+			else if (compTime.isNegative())
+				error += "Please enter a positive estimated completion time! ";
+		}
+		if (error.trim().length() > 0)
+			throw new InvalidInputException(error);
+
+		if (aStudent.getStudentRole(0) instanceof model.NoviceStudent)
+		{
+			anAssignment.setName(name);
+			anAssignment.setCourse(course);
+			anAssignment.setDueDate(dueDate);
+			anAssignment.setGradeWeight(gradeWeight);
+			return true;
+		} else if (aStudent.getStudentRole(0) instanceof model.ExperiencedStudent)
+		{
+			anAssignment.setName(name);
+			anAssignment.setCourse(course);
+			anAssignment.setDueDate(dueDate);
+			anAssignment.setCompletionTime(compTime);
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+
+	public boolean completeAssignment(Assignment anAssignment)
+	{
+		boolean currentState = anAssignment.getIsCompleted();
+		anAssignment.setIsCompleted(!currentState);
+		return true;
+	}
 	
 	public boolean removeAssignment(Student aStudent, Assignment anAssignment) throws InvalidInputException {
 		
@@ -173,8 +239,24 @@ public class DueThisController
 		
 		return true;
 	}
-	
-	
+
+	public boolean removeEvent(Event anEvent, Student aStudent) throws InvalidInputException
+	{
+		boolean legalRemove = aStudent.equals(anEvent.getStudent());
+
+		String error = "";
+
+		if (legalRemove)
+		{
+			anEvent.delete();
+		} else
+		{
+			error += "This event does not belong to this student! ";
+			throw new InvalidInputException(error);
+		}
+
+		return legalRemove;
+	}
 	
 	//when you click the save button on the availabilities page it runs this
 	public boolean updateAvailabilities(Student aStudent, int sunday, int monday, int tuesday, int wednesday, int thursday, int friday, int saturday) throws InvalidInputException{ 
